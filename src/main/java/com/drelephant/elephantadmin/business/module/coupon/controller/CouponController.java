ea25496eaedc7fac15731c8267f0a422c7a8bad6
yf-pc\yf <ffiing@qq.com>
2018-10-22 22:01:58 +0800

    优惠券部分- 删除多余代码

--- src/main/java/com/drelephant/elephantadmin/business/module/coupon/controller/CouponController.java
+++ src/main/java/com/drelephant/elephantadmin/business/module/coupon/controller/CouponController.java
@@ -1,26 +1,12 @@
 package com.drelephant.elephantadmin.business.module.coupon.controller;
 
-import com.drelephant.elephantadmin.business.module.coupon.CouponLimitVo;
-import com.drelephant.elephantadmin.business.module.coupon.service.CouponService;
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Api(tags = "优惠券")
 @RestController
 @RequestMapping("coupon")
 public class CouponController {
-
-    @Autowired
-    private CouponService couponService;
-
-    @GetMapping("/listByCouponInfoCode")
-    public  List<CouponLimitVo> listByCouponInfoCode(String couponInfoCode) {
-        return couponService.listByCouponInfoCode(couponInfoCode);
-    }
 
 }
--- src/main/java/com/drelephant/elephantadmin/business/module/coupon/service/CouponService.java
+++ src/main/java/com/drelephant/elephantadmin/business/module/coupon/service/CouponService.java
@@ -1,12 +1,6 @@
 package com.drelephant.elephantadmin.business.module.coupon.service;
 
-import com.drelephant.elephantadmin.business.module.coupon.CouponLimitVo;
-
-import java.util.List;
-
 public interface CouponService {
-
-    List<CouponLimitVo> listByCouponInfoCode(String couponInfoCode);
 
 
 }
